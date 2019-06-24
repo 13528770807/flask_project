67bb21dc4052bf9cb258abb384b1275f7ec40337
13528770807 <zqiang_0303@outlook.com>
2019-06-24 18:05:24 +0800

    正则匹配路由/异常抛出

--- qiang00_before_project/qiang01_view_and_route/q03_re_match_route.py
+++ qiang00_before_project/qiang01_view_and_route/q03_re_match_route.py
@@ -4,8 +4,8 @@ from werkzeug.routing import BaseConverter
 
 class RegexConverter(BaseConverter):
     def __init__(self, url_map, *args):
-        print(url_map)
-        print(args)
+        # print(url_map)
+        # print(args)
         super(RegexConverter, self).__init__(url_map)
         self.regex = args[0]
 
@@ -17,13 +17,18 @@ class RegexConverter(BaseConverter):
 
 app = Flask(__name__)
 app.url_map.converters['re'] = RegexConverter
-print(app)
+# print(app)
 
 
 @app.route('/demo1/<re("[0-9]{3}"):user_id>')
 def demo1(user_id):
     print('user_id_int', type(user_id))
-    return 'id: %d' % user_id
+    return 'id: %s' % user_id
+
+
+# @app.errorhandler(ValueError)
+# def value_error(e):
+#     return '不完整的格式'
 
 
 if __name__ == "__main__":
