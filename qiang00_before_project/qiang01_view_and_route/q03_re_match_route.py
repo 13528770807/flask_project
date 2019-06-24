9479b83d92a1aee85d6d9617e33a9568a4c0c2ce
13528770807 <zqiang_0303@outlook.com>
2019-06-24 17:44:22 +0800

    正则匹配路由

---
+++ qiang00_before_project/qiang01_view_and_route/q03_re_match_route.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from werkzeug.routing import BaseConverter
+
+
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *args):
+        print(url_map)
+        print(args)
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = args[0]
+
+    def to_python(self, value):
+        print('value:', value)
+        print('user_id_str:', type(value))
+        return int(value)
+
+
+app = Flask(__name__)
+app.url_map.converters['re'] = RegexConverter
+print(app)
+
+
+@app.route('/demo1/<re("[0-9]{3}"):user_id>')
+def demo1(user_id):
+    print('user_id_int', type(user_id))
+    return 'id: %d' % user_id
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
